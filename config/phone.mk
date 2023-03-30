7ee7ed9b7a992ed74ec02838a194dbb09016b6aa
Nanhumly <admin@warbler.cc>
2023-03-30 12:13:09 +0000

    config: Build Aperture camera app
    Signed-off-by: Nanhumly <admin@warbler.cc>

--- config/phone.mk
+++ config/phone.mk
@@ -11,6 +11,10 @@ $(call inherit-product, vendor/exthm/config/common.mk)
 PRODUCT_COPY_FILES += \
     vendor/exthm/prebuilt/etc/apns-conf.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/apns-conf.xml
 
+# Apps
+PRODUCT_PACKAGES += \
+    Aperture
+
 # Telephony packages
 PRODUCT_PACKAGES += \
     messaging \
